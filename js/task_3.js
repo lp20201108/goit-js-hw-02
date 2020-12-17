9ff9949b90f51522d862cd7b4d628e295c86844e
Liudmila Popescu <liudmila.popescu91@gmail.com>
2020-12-17 15:04:59 +0000

    task 3 done

--- js/task_3.js
+++ js/task_3.js
@@ -1,14 +1,30 @@
-// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку 
+//(в строке будут только слова и пробелы) 
+// и возвращает самое длинное слово в этой строке.
 
-// const findLongestWord = function(string) {
-//   // твой код
-// };
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+const findLongestWord = function (str) {
+   
+  let strSplit = str.split(' ');
 
-// console.log(findLongestWord('Google do a roll')); // 'Google'
+  let longestWord = ''; 
 
-// console.log(findLongestWord('May the force be with you')); // 'force'
+  for (let i = 0; i < strSplit.length; i++) {
+  
+    if (strSplit[i].length > longestWord.length) {
+      
+      longestWord = strSplit[i];
+    }
+
+  }
+    return longestWord;
+};
+  
+  
+console.log(findLongestWord('Google do a roll')); 
+  
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); 
+
+console.log(findLongestWord('May the force be with you'));
+ 
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
