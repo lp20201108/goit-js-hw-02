3e386c9cce6471ceebdbcda4062986806234b6b3
Liudmila Popescu <liudmila.popescu91@gmail.com>
2020-12-17 09:05:37 +0000

    almost done

--- js/task_5.js
+++ js/task_5.js
@@ -1,16 +1,25 @@
-// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку. Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale.Если нашли зарещенное слово то функция 
+// возвращает true, если запрещенных слов нет функция возвращает false.Слова в строке могут быть в произвольном регистре.
 
-// const checkForSpam = function(message) {
-//   // твой код
-// };
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(checkForSpam('Latest technology news')); // false
 
-// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+const checkForSpam = function (message) {
 
-// console.log(checkForSpam('Get best sale offers now!')); // true
+  let lowerMessage = message.toLowerCase();
+  console.log(lowerMessage);
 
-// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+  return lowerMessage.includes("spam") || lowerMessage.includes("sale");
+
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
